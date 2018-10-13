cbd1fb709e73471e59ab2b53d767f0d81b389aba
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-10-13 10:57:20 +0200

    project initialization

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
+const router = require('./routes/index');
+
+
+app.set(path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(flash())
+
+app.use('/', router);
+
+module.exports = app;
